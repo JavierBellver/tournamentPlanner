0298066b8873fee4b0e2390fb2ffc81ed9277e5f
Javier Bellver García <jbellver271@gmail.com>
2016-10-11 13:31:56 +0200

    Creado servidor base

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+
+app.get('*', function(req, res){
+	res.send("Bienvenido a tournamentPlanner")
+})
+
+app.listen(3000, function() {
+	console.log("Inicialización de servidor de tournamentPlanner")
+})
