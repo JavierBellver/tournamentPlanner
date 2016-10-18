abaf937ea0b6de47a20fc08cf059288792782f24
Javier Bellver García <jbellver271@gmail.com>
2016-10-18 13:25:25 +0200

    Añadidos listeners a las llamadas REST de la primera version

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ Web planner for tournaments made with node.js on the server side and js,jquery a
 1 to 4 : CRUD Tournaments
 5 to 8 : CRUD Organizador
 9 to 10: CR Participante. Participante se puede apuntar a torneos existentes.
+
+## Para iniciar la aplicación
+
+	-Iniciar una instancia de mongodb mongod --dbpath /home/jb/ADI/Practica1/tournamentPlanner --storageEngine=mmapv1
+	-
+
--- index.js
+++ index.js
@@ -2,13 +2,76 @@ var express = require('express');
 var app = express();
 
 var bodyParser = require('body-parser');
+var assert = require('assert');
 
+var MongoClient = require('mongodb').MongoClient;
+var MongoUrl = 'mongodb://localhost:27017/test'
 
+var tournaments = [];
+
+app.get('/api/tournaments')
 
 app.get('*', function(req, res){
 	res.send("Bienvenido a tournamentPlanner")
-})
+});
+
+app.get('/api/tournaments', function(req, res){
+
+});
+
+app.get('/api/tournaments/:id', function(req, res) {
+
+});
+
+app.post('/api/tournaments', function(req, res){
+
+});
+
+app.put('/api/tournaments/:id', function(req, res){
+
+});
+
+app.delete('/api/tournaments/:id', function(req, res) {
+
+});
+
+app.get('/api/organizer', function(req, res){
+
+});
+
+app.get('/api/organizer/:id', function(req, res){
+
+});
+
+app.post('/api/organizer', function(req, res){
+
+});
+
+app.put('/api/organizer/:id', function(req, res){
+
+});
+
+app.delete('/api/organizer/:id', function(req, res){
+
+});
+
+app.get('/api/tournaments/:id/competitors', function(req, res){
+
+});
+
+app.post('/api/tournaments/:id/competitors', function(req, res){
+
+});
+
+
+
 
 app.listen(3000, function() {
 	console.log("Inicialización de servidor de tournamentPlanner")
+	MongoClient.connect(MongoUrl, function(error, db){
+		assert.equal(null, error);
+		console.log("Conectado a la BD de MongoDB");
+
+		db.close();
+	})
 })
