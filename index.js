cbc480430399a3808a9fb18c3c607a681d614e18
Javier Bellver García <jbellver271@gmail.com>
2017-01-02 18:13:43 +0100

    Configured babel+react+browserify

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ var ObjectId = require('mongodb').ObjectId;
 var Server = require('mongodb').Server;
 var MongoUrl = 'mongodb://tournamentplanneruser:tournamentplannerpassword@ds139197.mlab.com:39197/heroku_vgr65f61'
 var db = new Db('heroku_vgr65f61', new Server('ds139197.mlab.com',39197));
+var jwt = require('jwt-simple');
 
 app.use(bodyParser.json());
 app.set('port', (process.env.PORT || 3000));
@@ -33,6 +34,11 @@ app.post('/login', function(req, res){
 		res.status(400);
 		res.send("Error, parametros incorrectos")
 	}
+});
+
+app.get('/logout', function(req, res){
+	req.session.destroy();
+	res.send('logout');
 });
 
 app.get('/api/tournaments', function(req, res){
